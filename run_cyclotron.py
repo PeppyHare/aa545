3d16f00e20590b3a1aa6907236850389bee26f5c
Evan <bluhme3@gmail.com>
2021-05-02 00:59:19 -0700

    Verify cyclotron frequency

--- run_cyclotron.py
+++ run_cyclotron.py
@@ -13,7 +13,7 @@ from util import count_crossings
 
 
 class CyclotronConfiguration(Configuration):
-    N = 2
+    N = 1
     M = 64
     x_min = -np.pi
     x_max = np.pi
@@ -22,19 +22,22 @@ class CyclotronConfiguration(Configuration):
     vy_min = -1.2
     vy_max = 1.2
     wp = 1.0
-    wc = 1.0
+    wc = 2.0
     dt = 0.001
     n_periods = 8
     markersize = 10
     max_history_steps = 1000
 
     def set_initial_conditions(self):
-        self.initial_x = np.zeros(2)
-        self.initial_vx = np.zeros(2)
-        self.initial_vy = np.array([1, -1])
+        self.initial_x = np.zeros(1)
+        self.initial_vx = np.zeros(1)
+        self.initial_vy = np.ones(1)
 
 
 c = CyclotronConfiguration()
 m = PicModel(c)
 m.run()
+d = m.d
+freq = count_crossings(d.ke_hist) / 4 / (c.n_periods / c.wp)
+print(f"Measured frequency: {freq:.2f}, wc: {c.wc:.2f}")
 plots.animate_phase_space(m, plot_title="Cyclotron Motion", repeat=True)
